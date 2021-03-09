90339b1e397796a6f9c7e1f1d979518593d25a9a
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2021-03-09 13:03:31 -0300

    Added code from challenge 01.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,31 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  function handleCreateNewTask() {        
+    if(newTaskTitle){
+      const newTask = {
+        id: Math.floor(Math.random() * 10000),
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map((task) => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete
+      }
+      return task;
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => {
+      return task.id !== id;
+    }))
   }
 
   return (
